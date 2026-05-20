2c23f8fc624c3241d1be284c452e415d47c6e5ee
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-05-20 06:33:59 -0500

    feat: console log para uso de variables

--- src/clean-code/01-names.ts
+++ src/clean-code/01-names.ts
@@ -13,6 +13,7 @@
     
     // Archivos marcados para borrar - files to delete
     const filesToDelete = files.map( x => x.toDelete );
+    console.log(`Archivos marcados para borrar: ${filesToDelete.filter(Boolean).length}`);
     
     class AbstractUser { };
     class UserMixin { };
@@ -28,23 +29,30 @@
         
     // día de hoy - today
     const currentDate = new Date();
+    console.log(`Fecha actual: ${currentDate}`);
     
     // días transcurridos - elapsed time in days
     const totalDays: number = 23;
+    console.log(`Días transcurridos: ${totalDays}`);
     
     // número de archivos en un directorio - number of files in directory
     const totalFiles = 33;
+    console.log(`Número de archivos: ${totalFiles}`);
     
     // primer nombre - first name
     const name = 'Fernando';
+    console.log(`Nombre: ${name}`);
     
     // primer apellido - last name
     const lastName = 'Herrera';
+    console.log(`Apellido: ${lastName}`);
 
     // días desde la última modificación - days since modification
     const daysSinceModification = 12;
+    console.log(`Días desde la última modificación: ${daysSinceModification}`);
     
     // cantidad máxima de clases por estudiante - max classes per student
     const maxClassesPerStudent = 6;
+    console.log(`Cantidad máxima de clases por estudiante: ${maxClassesPerStudent}`);
 
 })();
