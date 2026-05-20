dc201073973d960193220805716638e9f57462e6
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-05-20 06:17:20 -0500

    init: primer commit del proyecto

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Clean Code and SOLID</h1>
+    <span>Revisar la consola de JS</span>
+  </div>
+`
