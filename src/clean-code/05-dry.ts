dbe987cb2dcd89cbcee207b0bb5fb5691cdf86be
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-05-21 07:26:03 -0500

    feat: aplicando DRY

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -6,14 +6,36 @@ class Product {
         public price: number = 0,
         public size: Size = ''
     ){}
+
+    isProductReady() : boolean{
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if(this[key].length <= 0) throw new Error(`Product ${key} is required`);
+                    break;
+                case 'number':
+                    if(this[key] <= 0) throw new Error(`Product ${key} is required`);
+                    break;
+                default:
+                    throw new Error(`Product ${key} has an invalid type`);
+            }
+        }
+        return true;
+    }
+
     toString(){
-        if(this.name.length <= 0) throw new Error('Product name is required');
-        if(this.price <= 0) throw new Error('Product price is required');
-        if(this.size.length <= 0) throw new Error('Product size is required');
-        return `${this.name} (${this.price.toFixed(2)}), ${this.size}`;
+        //NO DRY
+        //if(this.name.length <= 0) throw new Error('Product name is required');
+        //if(this.price <= 0) throw new Error('Product price is required');
+        //if(this.size.length <= 0) throw new Error('Product size is required');
+        
+        //DRY
+        return this.isProductReady()
+        ? `${this.name} (${this.price.toFixed(2)}), ${this.size}` 
+        : '';
     }
 }
 (()=>{
-    const bluePants = new Product('Laptop', 100, 'M');
+    const bluePants = new Product('Jeans', 100, 'M');
     console.log(bluePants.toString());
 })();
