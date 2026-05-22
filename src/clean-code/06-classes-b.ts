5e0ab98f590baf11fdac7b427be175aa97d71680
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2026-05-21 19:59:32 -0500

    feat: problematica de herencia en SOLID

---
+++ src/clean-code/06-classes-b.ts
@@ -0,0 +1,56 @@
+(() => {
+    type Gender = 'M' | 'F';
+
+    class Person {
+    constructor(
+        public name: string,
+        public gender: Gender,
+        public birthDate: Date
+    ){}
+   }
+
+   class User extends Person {
+    public lastAccess: Date;
+    constructor(
+        public email: string,
+        public role: string,
+        name: string,
+        gender: Gender,
+        birthDate: Date
+    ){
+        super(name, gender, birthDate);
+        this.lastAccess = new Date();
+    }
+
+    checkCredentials(){
+        return true;
+    }
+   }
+
+   class UserSettings extends User{
+    constructor(
+        public workingDirectory: string,
+        public lastOpenFolder: string,
+        email: string,
+        role: string,
+        name: string,
+        gender: Gender,
+        birthDate: Date
+    ){
+        super(email, role, name, gender, birthDate);
+    }
+   }
+
+   const userSettings = new UserSettings(
+       '/home/user',
+       '/home/user/documents',
+       'john.doe@example.com',
+       'user',
+       'Ricardo Metzger',
+       'M',
+       new Date('1994-11-02')
+   );
+
+   console.log({userSettings});
+
+})();
